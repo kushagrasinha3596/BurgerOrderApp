ec5f63029997731c9a66fcbbbbac243fe60b291e
Kushagra Sinha <kushagra.sinha@comprotechnologies.com>
2020-03-21 13:01:22 -0700

    Adding react signup template for sigining up a user

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,5 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
 
 export const authStart = () => {
     return {
@@ -21,8 +22,21 @@ export const authFail = (error) => {
 }
 
 export const auth = (email, password) => {
+    debugger
     return (dispatch) => {
-        
+        dispatch(authStart());
+        const authData = {
+            email: email,
+            password: password,
+            returnSecureToken: true
+        }
+        axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCvJ7RjNyH92MPwllMd5jxhF4Ci2OFx8vw',authData)
+        .then((response) => {
+            dispatch(authSuccess(response.data));
+        })
+        .catch((error)=> {
+            dispatch(authFail(error));
+        })
     }
 }
 
